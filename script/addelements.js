c0586d52be72c739150f70585c08a1460bc6195b
unknown <kovacs.daniel182@gmail.com>
2020-08-24 10:47:35 +0200

    nothing happend.

--- script/addelements.js
+++ script/addelements.js
@@ -1,59 +1,79 @@
-let picturesdata={
-    name:["Huawei P20 lite","Huawei Psmart"],
-    link:[" https://www.gsmarena.com/huawei_p20_lite-9098.php","https://www.gsmarena.com/huawei_p_smart_2019-9409.php"],
-    img:["img/p20_lite.webp","img/huawi_p_smart.jpg"]
+let phonePicturesData = {
+    name: ["Huawei P20 lite", "Huawei_P20", "Huawei Psmart 2019",],
+    link: [" https://www.gsmarena.com/huawei_p20_lite-9098.php", "https://www.gsmarena.com/huawei_p_smart_2019-9409.php"],
+    img: ["img/p20_lite.webp", "img/huawi_p_smart.jpg"]
 
 
-}
+};
+
+function iteration()
 {
+   for(let k in phonePicturesData)
+   {
+       
+       
+       for(let data in phonePicturesData[k])
+       {
+            console.log(data)
+
+            
+
+           
+       }
+   }
    
 }
-
-
-function setclassName()
-{
-    let select=document.querySelectorAll("div.col-2:first-child");
-    
-        for (let k in select)
-        {
-
-        select[k].className+=" phone";
-        }
-    
-
-}
-setclassName();
-
-function createPicture()
-{
-
-
-let selectelement=document.querySelector("#pSmart");
-let createelement=createAnyElement("a",{href:"https://www.gsmarena.com/huawei_p20_lite-9098.php",target:"blank"});
+iteration();
 
 
 
-createelement.innerHTML='<img src="img/p20_lite.webp" alt="p20lite">';
+
+function createPicture( )
+ {
 
 
-return selectelement.appendChild(createelement);
+
+    let createelement = createAnyElement("a", { href: "https://www.gsmarena.com/huawei_p20_lite-9098.php", target: "blank" });
+
+
+
+    createelement.innerHTML = '<img src="img/p20_lite.webp" alt="p20lite">';
+
+
+
+    return createelement;
+
 
 
 }
-createPicture();
 
-function createAnyElement(name,setAttributes)
-{
-    let cElement=document.createElement(name);
-    for(k in setAttributes)
-    {
-        cElement.setAttribute(k,setAttributes[k])
+
+function setpicture() {
+    let select = document.querySelectorAll(".col-2:first-child");
+    
+    for (let k in select) {
+
+        console.log(k);
+        console.log(select);
+
+
+        select[k].appendChild(createPicture())
+
+
+    }
+
+
+}
+
+
+
+function createAnyElement(name, setAttributes) {
+    let cElement = document.createElement(name);
+    for (k in setAttributes) {
+        cElement.setAttribute(k, setAttributes[k])
     }
 
     return cElement;
 
 }
- function setpictutes()
- {
 
- }
